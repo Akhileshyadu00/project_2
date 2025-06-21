7560d968d520da0b07b31855424d53e7df58907f
Akhileshyadu00 <akhileshyadu001@gmail.com>
2025-06-22 00:33:43 +0530

    first commit

--- server.js
+++ server.js
@@ -14,10 +14,9 @@ app.use(express.json());
 // Routes
 app.use('/', userRoutes); // ✅ Register routes
 
-// MongoDB connection
+// MongoDB connection. //also cloud atlas
 mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+ 
 })
   .then(() => {
     console.log('✅ Connected to MongoDB');
@@ -26,3 +25,7 @@ mongoose.connect(process.env.MONGODB_URI, {
   .catch(err => {
     console.error(' MongoDB connection error:', err);
   });
+
+  //akhileshyadu001
+  //7uN7K2RB6EJ2mYYi
+  //mongodb+srv://<db_username>:<db_password>@cluster0.x3ne1tq.mongodb.net/
